7f83a91be10ac8bcd48c1affc5db910b0e82a648
John Christensen <jechristens3@wisc.edu>
2024-10-08 15:28:16 -0500

    Add lsp diagnostic display to bufferline tabs

--- lua/custom/plugins/ui.lua
+++ lua/custom/plugins/ui.lua
@@ -59,7 +59,15 @@ return {
     event = 'VeryLazy',
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-      require('bufferline').setup {}
+      require('bufferline').setup {
+        options = {
+          diagnostics = 'nvim_lsp',
+          diagnostics_indicator = function(count, level)
+            local icon = level:match 'error' and ' ' or ' '
+            return ' ' .. icon .. count
+          end,
+        },
+      }
       vim.keymap.set('n', '<Tab>', '<cmd>bn<cr>', { desc = 'next buffer' })
       vim.keymap.set('n', '<S-Tab>', '<cmd>bp<cr>', { desc = 'previous buffer' })
     end,
