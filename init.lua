a9f13fd177f9008896853048ef866f077800088b
John Christensen <jechristens3@wisc.edu>
2024-10-08 15:39:04 -0500

    Remove fennel_ls, since it has a dependency on luarocks I don't wan't to need to have that on any machine I have nvim on

--- init.lua
+++ init.lua
@@ -475,7 +475,7 @@ require('lazy').setup({
           },
         },
         ts_ls = {},
-        fennel_ls = {},
+        --fennel_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
