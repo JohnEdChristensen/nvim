21defaa4a2cfaaefa708571a7dbf22d5925a15a1
John Christensen <johnedchristensen@gmail.com>
2024-11-19 11:04:48 -0600

    change ruff_lsp to ruff

--- init.lua
+++ init.lua
@@ -434,7 +434,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         wgsl_analyzer = {},
-        ruff_lsp = {
+        ruff = {
           -- unsure of format
           settings = {
             lint = {
@@ -552,7 +552,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, python = true }
+        local disable_filetypes = { c = true, cpp = true, python = false }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
